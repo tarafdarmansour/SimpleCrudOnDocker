1e07aae4b4ae36c50e447039c7f7f88f269457e9
tarafdarmansour <tarafdar.mansour@gmail.com>
2021-05-30 09:43:02 +0430

    adding dbcontect

--- SimpleCrudOnDocker/Models/Product.cs
+++ SimpleCrudOnDocker/Models/Product.cs
@@ -7,5 +7,9 @@ namespace SimpleCrudOnDocker.Models
 {
     public class Product
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Price { get; set; }
+        public string Description { get; set; }
     }
 }
